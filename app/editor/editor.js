5685fd591f9f8548437b76620a2aa8822784b4c3
0aoq <73413330+0aoq@users.noreply.github.com>
2021-09-14 20:27:50 -0400

    Fix URL issue when loading emmet

--- app/editor/editor.js
+++ app/editor/editor.js
@@ -91,7 +91,7 @@ monaco.editor.defineTheme('theme', {
     }
 })
 
-if (window.location.href !== "https://0aoq.github.io/ubiquitous-engine/app/") {
+if (window.location.href !== "https://0aoq.github.io/WebEditor/app/") {
     console.log("Emmet enabled!")
     emmetMonaco.emmetHTML()
     emmetMonaco.emmetCSS()
